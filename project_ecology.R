fa491d2c7da57b81f7dde57a54dc8e9887f853ba
Ilmira255 <57386296+Ilmira255@users.noreply.github.com>
2019-12-16 18:45:29 +0300

    Update project_ecology.R
    update

--- project_ecology.R
+++ project_ecology.R
@@ -5,7 +5,7 @@ library("downloader")
 
 download.file('http://api.worldbank.org/v2/en/indicator/EN.ATM.CO2E.PC?downloadformat=csv', dest = 'dataco21.zip', mode = 'wb')
 unzip(zipfile="dataco21.zip",exdir="./data")
-setwd("C:/Users/Ильмира/Documents/data")
+setwd("C:/Users/Г€Г«ГјГ¬ГЁГ°Г /Documents/data")
 
 raw_d <- readChar("API_EN.ATM.CO2E.PC_DS2_en_csv_v2_566534.csv", file.info("API_EN.ATM.CO2E.PC_DS2_en_csv_v2_566534.csv")$size)
 raw_d <- gsub('"', ' ', raw_d)
@@ -25,11 +25,11 @@ ggplot(data=data2, aes(x = year, y = World_emissions_CO2)) +
   geom_line(color = "green", size = 2) +
   geom_point(size = 2, shape = 15, color = colors) +
   geom_area(alpha = 0.5) +
-  ggtitle('Динамика выбросов СО2 в мире, 1960 - 2014 гг.',
-          subtitle = 'За последние 54 года выбросы значительно увеличились')+
-  xlab('Год') +
-  ylab('Тонны СО2') +
+  ggtitle('Р”РёРЅР°РјРёРєР° РІС‹Р±СЂРѕСЃРѕРІ РЎРћ2 РІ РјРёСЂРµ, 1960 - 2014 РіРі.',
+          subtitle = 'Р—Р° РїРѕСЃР»РµРґРЅРёРµ 54 РіРѕРґР° РІС‹Р±СЂРѕСЃС‹ Р·РЅР°С‡РёС‚РµР»СЊРЅРѕ СѓРІРµР»РёС‡РёР»РёСЃСЊ')+
+  xlab('Р“РѕРґ') +
+  ylab('РўРѕРЅРЅС‹ РЎРћ2') +
   annotate("rect", xmin = 2000, xmax = 2014, ymin = 25000000, ymax = 38000000,  alpha = .2, color = 'red3', size = 0.1) +
-  annotate("text", x = 2007, y = 40000000, label = "Резкое повышение", color = 'red3', size=5) +
+  annotate("text", x = 2007, y = 40000000, label = "ГђГҐГ§ГЄГ®ГҐ ГЇГ®ГўГ»ГёГҐГ­ГЁГҐ", color = 'red3', size=5) +
   theme_bw() 
 
